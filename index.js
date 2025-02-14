721eafe68af906f753226cb28538369c2c26176a
Abhijeetp12 <130688454+Abhijeetp12@users.noreply.github.com>
2025-02-14 17:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import {nanoid} from 'nanoid';
 
 const app=express();
-const port=3000;
+const port = process.env.PORT || 3000;
 let blogpost=[];
 let bid;
 let signal;
